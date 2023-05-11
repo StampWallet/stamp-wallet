f4992df17cf32f493c9a8d28400706b4b846fdea
dawba <dawid.bania@spinney.io>
2023-05-11 02:43:36 +0200

    constants update: colors, paths, types, StyleBase

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,4 @@
+// Apps colour palette
 export const swWhite = '#FAF9F6';
 export const swDarkBlue = '#0F6BAE';
 export const swLightBlue = '#50AAEB';
@@ -7,3 +8,4 @@ export const swViolet = '#C9CDFF';
 export const swPaleViolet = '#ECEFFF';
 export const swPaleGreen = 'rgba(123,255,120,0.5)';
 export const swStrongGreen = '#7BFF78';
+export const swRed = '#d00404';
