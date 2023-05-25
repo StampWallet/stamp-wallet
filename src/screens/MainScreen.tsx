b5f72e38bcee71109638ba1ce496f1346c579124
dawba <dawid.bania@spinney.io>
2023-05-25 09:36:46 +0200

    refactor main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, StatusBar, FlatList, Text, StyleSheet } from 'react-native';
+import { View, StatusBar, StyleSheet } from 'react-native';
 
 import TopBar from '../components/Bars/TopBar';
-import CardTile from '../components/Cards/CardTile';
-import ListItemSeparator from '../components/Miscellaneous/ListItemSeparator';
+import CardList from '../components/Cards/CardList';
 import CustomButton from '../components/Miscellaneous/CustomButton';
 import StyleBase from '../styles/StyleBase';
 import SearchBar from '../components/Bars/SearchBar/SearchBar';
@@ -13,74 +12,15 @@ import { OptionKey } from '../components/Bars/SearchBar/OptionRow';
 
 import filterCards from '../utils/filterCards';
 
+import cards from '../mockData/cards';
+
 /*
 todo:
       make search maintain proper screen composition
+
+todo:
+      get cards from database entity
 */
-
-const cards = [
-  {
-    name: 'Biedronka',
-    image: require('../assets/images/biedronka_homepage.jpg'),
-    type: 'virtual',
-  },
-  {
-    name: 'Lidl',
-    image: require('../assets/images/lidl.png'),
-    type: 'virtual',
-  },
-  {
-    name: 'Akbar brothers',
-    image: require('../assets/images/akbar.png'),
-    type: 'real',
-  },
-  {
-    name: 'Costadoro Coffee',
-    image: require('../assets/images/costadoro.png'),
-    type: 'virtual',
-  },
-  {
-    name: 'Decathlon',
-    image: require('../assets/images/decathlon.png'),
-    type: 'real',
-  },
-  {
-    name: 'Deichmann',
-    image: require('../assets/images/deichmann.png'),
-    type: 'real',
-  },
-  {
-    name: 'Espirit',
-    image: require('../assets/images/espirit.png'),
-    type: 'virtual',
-  },
-  {
-    name: 'Kaufland',
-    image: require('../assets/images/kaufland.png'),
-    type: 'real',
-  },
-  {
-    name: 'Maggi spices',
-    image: require('../assets/images/maggi.png'),
-    type: 'virtual',
-  },
-  {
-    name: 'Pollo Feliz',
-
-    image: require('../assets/images/pollofeliz.png'),
-    type: 'real',
-  },
-  {
-    name: 'Roblox',
-    image: require('../assets/images/roblox.png'),
-    type: 'real',
-  },
-  {
-    name: 'Stock',
-    image: require('../assets/images/stock.png'),
-    type: 'virtual',
-  },
-];
 
 export default function MainScreen({ navigation }) {
   const [cardQuery, setCardQuery] = useState('');
@@ -88,6 +28,7 @@ export default function MainScreen({ navigation }) {
   const [filter, setFilter] = useState<OptionKey | null>(null);
   const [filteredCards, setFilteredCards] = useState(cards);
 
+  // filters cards based on search query + current filter
   useEffect(() => {
     let cardList = cards;
 
@@ -113,7 +54,6 @@ export default function MainScreen({ navigation }) {
         onPressRight={() => setFilterDropdownVisibility((prevState) => !prevState)}
       />
       <SearchBar onChangeText={setCardQuery} value={cardQuery} />
-
       {isFilterDropdownOpen && (
         <SortOptions
           setFilter={setFilter}
@@ -125,23 +65,14 @@ export default function MainScreen({ navigation }) {
         style={[
           StyleBase.container,
           isFilterDropdownOpen && styles.listOpacity,
-          styles.listContainer,
+          StyleBase.listContainer,
         ]}
       >
-        {filteredCards.length ? (
-          <FlatList
-            data={filteredCards}
-            renderItem={({ item }) => (
-              <CardTile image={item.image} onPress={() => navigation.push('CardInfoScreen')} />
-            )}
-            ItemSeparatorComponent={ListItemSeparator}
-          />
-        ) : (
-          <Text style={styles.emptyList}>No cards found</Text>
-        )}
+        <CardList cards={filteredCards} />
+        {/* TODO: swap this button with tap bars addition option*/}
         <CustomButton
-          onPress={() => navigation.push('BenefitManipulationScreen')}
-          title='To benefit manipulation'
+          onPress={() => navigation.push('CardAdditionScreen')}
+          title='To card addition'
         />
         <CustomButton
           onPress={() =>
@@ -164,9 +95,5 @@ const styles = StyleSheet.create({
   },
   listOpacity: {
     opacity: 0.5,
-  },
-  listContainer: {
-    display: 'flex',
-    justifyContent: 'flex-start',
   },
 });
