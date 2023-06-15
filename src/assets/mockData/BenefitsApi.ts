f65d12bdc6e9ec695167134150440ce630ffdd04
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2023-06-16 00:28:38 +0200

    added mock data consistent with api data

---
+++ src/assets/mockData/BenefitsApi.ts
@@ -0,0 +1,6 @@
+export const benefits = [
+  { publicId: '1', definitionId: '1' },
+  { publicId: '2', definitionId: '1' },
+  { publicId: '3', definitionId: '2' },
+  { publicId: '4', definitionId: '1' },
+];
