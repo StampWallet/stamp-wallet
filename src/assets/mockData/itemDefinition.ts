bee6db48b48bc11cbc9b6dd814ec3b82df356f15
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2023-06-16 00:39:17 +0200

    added mockData consistent with api

---
+++ src/assets/mockData/itemDefinition.ts
@@ -0,0 +1,24 @@
+export const itemDefinitions = [
+  {
+    publicId: '1',
+    name: 'Cool item1',
+    price: 10,
+    description: 'idk',
+    imageId: 'QWhDOXBvdTdhaTZQCg',
+    startDate: '2023-04-21T15:25:37+02:00',
+    endDate: '2024-04-21T17:25:37+02:00',
+    maxAmount: 2,
+    available: true,
+  },
+  {
+    publicId: '2',
+    name: 'Cool item2',
+    price: 10,
+    description: 'idk',
+    imageId: 'QWhDOXBvdTdhaTZQCg',
+    startDate: '2023-04-21T15:25:37+02:00',
+    endDate: '2024-04-21T17:25:37+02:00',
+    maxAmount: 2,
+    available: true,
+  },
+];
