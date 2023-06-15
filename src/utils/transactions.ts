ba5039fd4ebc74f0453138ee976249e09b71bfc1
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2023-06-16 00:29:18 +0200

    added transaction handling

---
+++ src/utils/transactions.ts
@@ -0,0 +1,57 @@
+import React from 'react';
+import * as api from '../api';
+import Auth from '../database/Auth';
+
+export const startTransaction = async (publicId: string, claimedBenefits: any[]) => {
+  const TA = new api.TransactionApi();
+  try {
+    const header = Auth.getAuthHeader();
+    const transactionResponse = await TA.startTransaction(
+      publicId,
+      { itemIds: claimedBenefits },
+      header
+    );
+    console.log(transactionResponse.data.Code, ' ', transactionResponse.data.publicId);
+  } catch (e) {
+    console.log('error:', e);
+  }
+};
+
+export const fetchStartedTransaction = async (
+  callbackFn: any, //temp
+  transactionCode: string
+) => {
+  const TsA = new api.TransactionsApi();
+  try {
+    const header = Auth.getAuthHeader();
+    const transactionResponse = await TsA.getTransactionDetails(transactionCode, header);
+    callbackFn(transactionResponse);
+  } catch (e) {
+    console.log('error: ', e);
+    callbackFn({});
+  }
+};
+
+//idk
+export const postTransaction = async (
+  //callbackFn jako snackbar?
+  //callbackFn: any, //temp
+  transactionCode: string,
+  transactionRequest: any
+) => {
+  const TsA = new api.TransactionsApi();
+  try {
+    const header = Auth.getAuthHeader();
+
+    const transactionResponse = await TsA.finishTransaction(
+      transactionCode,
+      transactionRequest,
+      header
+    );
+    console.log(transactionResponse.data.message);
+    return;
+  } catch (e) {
+    console.log('error: ', e);
+    return;
+  }
+};
