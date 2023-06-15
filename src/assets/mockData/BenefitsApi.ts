6cfd4a6ff3fb177142331abc947f807b74a1b8e9
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2023-06-16 00:32:21 +0200

    Revert "added mock data consistent with api data"
    This reverts commit f65d12bdc6e9ec695167134150440ce630ffdd04.

--- src/assets/mockData/BenefitsApi.ts
+++
@@ -1,6 +0,0 @@
-export const benefits = [
-  { publicId: '1', definitionId: '1' },
-  { publicId: '2', definitionId: '1' },
-  { publicId: '3', definitionId: '2' },
-  { publicId: '4', definitionId: '1' },
-];
